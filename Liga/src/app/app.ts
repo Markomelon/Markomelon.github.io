fe5db28012b4543cd8fd41c5c424f9e737875261
Markomelon <marko.schneider92@web.de>
2025-08-11 10:07:03 +0200

    Sorting table

--- Liga/src/app/app.ts
+++ Liga/src/app/app.ts
@@ -1,15 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatTab } from '@angular/material/tabs';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material/table';
+import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  score: number;
-  played: number;
-}
 
 const FAKE_DATA: PeriodicElement[] = [
   {position: 1, name: 'Nicole', score: 10, played: 5},
@@ -23,16 +18,54 @@ const FAKE_DATA: PeriodicElement[] = [
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatTabsModule, MatTab, MatTableModule],
+  imports: [RouterOutlet, MatTabsModule, MatTab, MatTableModule,MatSortModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
   
 })
+
+
 export class App {
   protected readonly title = signal('Liga');
   displayedColumns: string[] = ['position', 'name', 'score', 'played'];
   datasource = FAKE_DATA;
 }
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  score: number;
+  played: number;
+}
 
+/**
+@Component({
+  selector: 'table-sorting-example',
+  styleUrl: 'table-sorting-example.css',
+  templateUrl: 'table-sorting-example.html',
+  imports: [MatTableModule, MatSortModule],
+})
+export class TableSortingExample implements AfterViewInit {
+  private _liveAnnouncer = inject(LiveAnnouncer);
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+
+  announceSortChange(sortState: Sort) {
+  
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+}
+*/
 
